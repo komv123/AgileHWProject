ef2c0326220fb6c7fe8df664c2e9cea7096a8e08
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-10-19 23:19:48 +0200

    moved VGA to seperate module

---
+++ VGA/test/src/VGATester.scala
@@ -0,0 +1,9 @@
+import chisel3._
+import chisel3.simulator.EphemeralSimulator._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class VGATester extends AnyFlatSpec {
+  "VGA" should "pass" in {
+    println("HELLO FROM VGA TEST")
+  }
+}
