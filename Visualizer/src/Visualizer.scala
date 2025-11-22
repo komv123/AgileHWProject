8326fe6330bebad23f43e7440d0a64c989e1db7c
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-11-22 14:08:33 +0100

    added top module

---
+++ Visualizer/src/Visualizer.scala
@@ -0,0 +1,34 @@
+package visualizer
+
+import chisel3._
+
+import circt.stage.ChiselStage
+
+import vga._
+
+class Visualizer extends Module {
+  val io = IO(new Bundle {
+    val btnl, btnc, btnr = Input(Bool())
+    val red, green, blue = Output(UInt(4.W))
+    val horizontalSyncPulse, verticalSyncPulse = Output(Bool())
+  })
+
+  val clockFrequency = 100000000
+  val config = VGAConfig.vga640x480at60Hz
+  val vgaController = Module(new VGAController(config, clockFrequency))
+
+  io.horizontalSyncPulse := vgaController.io.horizontalSyncPulse
+  io.verticalSyncPulse := vgaController.io.verticalSyncPulse
+
+  io.red := Mux(io.btnl, "b1111".U, "b0000".U)
+  io.green := Mux(io.btnc, "b1111".U, "b0000".U)
+  io.blue := Mux(io.btnr, "b1111".U, "b0000".U)
+}
+
+object Main extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new Visualizer(),
+    args = Array("--target-dir", "rtl"),
+    firtoolOpts = Array("--lowering-options=disallowLocalVariables,disallowPackedArrays")
+  )
+}
