d876240b61a384d3a2ca8edba968eb2c3ea086f0
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-11-25 15:44:18 +0100

    added Visualizer test with picture output

--- Visualizer/test/src/VisualizerTester.scala
+++ Visualizer/test/src/VisualizerTester.scala
@@ -11,15 +11,37 @@ import java.awt.Color
 import javax.imageio.ImageIO
 
 class VisualizerTester extends AnyFlatSpec with Matchers with ChiselSim {
-  "Visulizer" should "pass" in {
+  "Visualizer" should "pass" in {
     val config = VisualizerConfig.default
     simulate(new Visualizer(config)) { dut =>
       val canvas = new BufferedImage(config.vga.horizontal.pixels, config.vga.vertical.pixels, BufferedImage.TYPE_INT_ARGB)
-      for (i <- 0 until canvas.getWidth()) {
-        for (j <- 0 until canvas.getHeight()) {
-          canvas.setRGB(i, j, 0xFF0000FF)
+
+      def stepPixelClock(n: Int) = {
+        dut.clock.step(n * (config.clockFrequency / config.vga.pixelFrequency))
+      }
+
+      def testHorizontalPeriod(line: Int) = {
+        for (pixel <- 0 until config.vga.horizontal.pixels) {
+          if (line < config.vga.vertical.pixels) {
+            val red = (dut.io.colors.red.peek().litValue.toInt << (8 - config.vga.colorDepth)) | ((1 << (8 - config.vga.colorDepth)) - 1)
+            val green = (dut.io.colors.green.peek().litValue.toInt << (8 - config.vga.colorDepth)) | ((1 << (8 - config.vga.colorDepth)) - 1)
+            val blue = (dut.io.colors.blue.peek().litValue.toInt << (8 - config.vga.colorDepth)) | ((1 << (8 - config.vga.colorDepth)) - 1)
+            canvas.setRGB(pixel, line, (0xFF << 24) + (red << 16) + (green << 8) + blue)
+          }
+          stepPixelClock(1)
+        }
+        for (i <- 0 until config.vga.horizontal.frontPorch + config.vga.horizontal.syncPulse + config.vga.horizontal.backPorch) {
+          stepPixelClock(1)
         }
       }
+
+      def testVerticalPeriod() = {
+        for (line <- 0 until config.vga.vertical.pixels + config.vga.vertical.frontPorch + config.vga.vertical.syncPulse + config.vga.vertical.backPorch) {
+          testHorizontalPeriod(line)
+        }
+      }
+
+      testVerticalPeriod()
       javax.imageio.ImageIO.write(canvas, "png", new java.io.File("drawing.png"))
     }
   }
