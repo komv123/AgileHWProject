6ffab5b0cf63199eb58bb71f914ec7603d4b4cf8
Karl Krause <karl@krauses.dk>
2025-11-25 16:42:12 +0100

    Pipeline test render 128 x 128

--- Pipeline/test/src/PipelineTest.scala
+++ Pipeline/test/src/PipelineTest.scala
@@ -108,4 +108,59 @@ class PipelineTest extends AnyFlatSpec with ChiselSim {
             writer.close()
         }
     }
+    "Pipeline" should "render 128 x 128 " in {
+        simulate(new Pipeline(128, 128)) { dut =>
+            val writer = new PrintWriter("output128_128.ppm")
+
+            // PPM header
+            writer.println("P3")
+            writer.println(s"128 128")
+            writer.println("15")
+
+            dut.io.xmid.poke(-5755256176L.S)
+            dut.io.ymid.poke(0L.S)
+            dut.io.zoom.poke(8589934592L.S)
+            dut.io.maxiter.poke(1000.U)
+
+            var framePointer = 0 
+
+            dut.io.new_params.poke(1.B)
+
+            dut.clock.step(5)
+            dut.io.new_params.poke(0.B)
+
+            for(i <- 0 until 16){ 
+              dut.io.framePointer.poke(framePointer.U)
+              //dut.io.bufferPointer.poke(bufferPointer.U)
+
+              breakable{for (i <- 0 until 1000000) {
+                  dut.clock.step(1)
+              }}
+
+              //if(i == 0){
+              //  dut.io.ReadData.request.valid.poke(true.B)
+              //  dut.clock.step(1)
+              //}
+
+              for (i <- 0 until 1024){
+                  dut.io.ReadData.request.valid.poke(true.B)
+
+                  val rgb = dut.io.ReadData.response.bits.readData.peek().litValue
+                  //println(f"RGB: ${rgb}%03X")
+                  val r = (rgb >> 8) & 0xF
+                  val g = (rgb >> 4) & 0xF
+                  val b = rgb & 0xF
+
+                  writer.println(s"$r $g $b")
+                  dut.clock.step(1)
+              }
+
+              dut.io.ReadData.request.valid.poke(false.B)
+
+              framePointer += 1024
+            } 
+
+            writer.close()
+        }
+    }
 }
