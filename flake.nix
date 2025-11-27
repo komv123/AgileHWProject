2c76a24b35e039259ac08b8df11962503e309d59
Mads Rumle Nordstrøm <madsrumlenordstrom@icloud.com>
2025-11-27 12:56:24 +0100

    added openxc7 synthesis flow

--- flake.nix
+++ flake.nix
@@ -3,13 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    openxc7.url = "github:openxc7/toolchain-nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, openxc7, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        openxc7Pkgs = openxc7.packages.${system};
+
         packages = with pkgs; [
           mill
           verilator
@@ -19,9 +22,48 @@
           gnumake
           jdk11_headless
         ];
+
+        synthPackages = with pkgs; [
+          yosys
+          ghdl
+          yosys-ghdl
+          openfpgaloader
+          pypy310
+          python312Packages.pyyaml
+          python312Packages.textx
+          python312Packages.simplejson
+          python312Packages.intervaltree
+        ];
+
+        openxc7Packages = with openxc7Pkgs; [
+          fasm
+          fpga-assembler
+          prjxray
+          nextpnr-xilinx
+        ];
       in {
         devShells = {
           default = pkgs.mkShell { name = "chisel"; inherit packages; };
+          openxc7 = pkgs.mkShell {
+            name = "openxc7";
+            packages = packages ++ synthPackages ++ openxc7Packages;
+            shellHook = nixpkgs.lib.concatStrings [
+              "export NEXTPNR_XILINX_DIR=" openxc7Pkgs.nextpnr-xilinx.outPath "\n"
+              "export NEXTPNR_XILINX_PYTHON_DIR=" openxc7Pkgs.nextpnr-xilinx.outPath "/share/nextpnr/python/\n"
+              "export PRJXRAY_DB_DIR=" openxc7Pkgs.nextpnr-xilinx.outPath "/share/nextpnr/external/prjxray-db\n"
+              "export PRJXRAY_PYTHON_DIR=" openxc7Pkgs.prjxray.outPath "/usr/share/python3/\n"
+              ''export PYTHONPATH=''$PYTHONPATH:''$PRJXRAY_PYTHON_DIR:'' 
+                openxc7Pkgs.fasm.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.textx.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.arpeggio.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.pyyaml.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.simplejson.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.intervaltree.outPath "/lib/python3.12/site-packages/:"
+                pkgs.python312Packages.sortedcontainers.outPath "/lib/python3.12/site-packages/:"
+                "\n"
+              "export PYPY3=" pkgs.pypy310.outPath "/bin/pypy3.10"
+            ];
+          };
         };
       });
 }
---
+++ synth/f4pga.mk
@@ -0,0 +1,67 @@
+THIS_FILE := $(firstword $(MAKEFILE_LIST))
+
+current_dir := ${SYNTHDIR}
+TOP         := $(TOP_MODULE)
+SOURCES     := $(RTL_SOURCES)
+
+# Artix-7 Nexys A7-100T device triplet
+TARGET    := arty_100
+BOARD     := nexys_a7_100
+DEVICE    := xc7a100t
+PACKAGE   := csg324
+SPEED     := -1
+PART      := xc7a100t-1csg324
+FPGA_FAM  := xc7
+
+BOARDDIR ?= $(SYNTHDIR)/boards
+SYNTHBUILDDIR ?= $(SYNTHDIR)/build
+
+XDC := ${BOARDDIR}/${BOARD}.xdc
+
+# F4PGA stuff
+F4PGA_INSTALL_DIR  := $(SYNTHDIR)/f4pga/tools
+F4PGA_EXAMPLES_DIR := $(SYNTHDIR)/f4pga/f4pga-examples
+F4PGA_TIMESTAMP    := "20220920-124259"
+F4PGA_HASH         := "007d1c1"
+
+.ONESHELL:
+SHELL = bash
+
+# Synthsize design
+.PHONY: synth
+synth:
+	export FPGA_FAM=$(FPGA_FAM)
+	export F4PGA_INSTALL_DIR=$(F4PGA_INSTALL_DIR)
+	source "${F4PGA_INSTALL_DIR}/${FPGA_FAM}/conda/etc/profile.d/conda.sh"
+	conda activate $(FPGA_FAM)
+	$(MAKE) -f $(THIS_FILE) all
+
+# Program FPGA
+.PHONY: program
+program:
+	export FPGA_FAM=$(FPGA_FAM)
+	export F4PGA_INSTALL_DIR=$(F4PGA_INSTALL_DIR)
+	source "${F4PGA_INSTALL_DIR}/${FPGA_FAM}/conda/etc/profile.d/conda.sh"
+	conda activate $(FPGA_FAM)
+	$(MAKE) -f $(THIS_FILE) download
+
+# Install F4PGA synthesis tools
+.PHONY: install
+install:
+	git clone "https://github.com/chipsalliance/f4pga-examples" ${F4PGA_EXAMPLES_DIR}
+	curl -L https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o conda_installer.sh
+	bash conda_installer.sh -u -b -p "${F4PGA_INSTALL_DIR}/${FPGA_FAM}/conda"
+	$(RM) conda_installer.sh
+	source "${F4PGA_INSTALL_DIR}/${FPGA_FAM}/conda/etc/profile.d/conda.sh"
+	conda env create -f $(FPGA_FAM)/environment.yml
+	conda env update -f $(FPGA_FAM)/environment.yml
+	mkdir -p $(F4PGA_INSTALL_DIR)/$(FPGA_FAM)
+	wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-install-xc7-${F4PGA_HASH}.tar.xz | tar -xJC $(F4PGA_INSTALL_DIR)/$(FPGA_FAM)
+	wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-xc7a100t_test-${F4PGA_HASH}.tar.xz | tar -xJC $(F4PGA_INSTALL_DIR)/$(FPGA_FAM)
+
+# Uninstall F4PGA synthesis tools
+.PHONY: uninstall
+uninstall:
+	$(RM) -rf $(F4PGA_INSTALL_DIR) $(F4PGA_EXAMPLES_DIR)
+
+-include ${F4PGA_EXAMPLES_DIR}/common/common.mk
