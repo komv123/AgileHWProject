2d74db9c06f3656aa2129b3acbf75a5fe2aac61b
willdew <william.brochner@gmail.com>
2025-11-29 22:37:13 +0100

    Changed default data width

--- UserInput/src/UserInput.scala
+++ UserInput/src/UserInput.scala
@@ -6,7 +6,7 @@ import Common._
 
 // Basic class for user input to output, for example the nexys a7 microphone
 // and or buttons
-class UserInputPosition(val dataWidth: Int = 64) extends Bundle {
+class UserInputPosition(val dataWidth: Int = 32) extends Bundle {
   val ymid = SInt(dataWidth.W)
   val xmid = SInt(dataWidth.W)
   val zoom = SInt(dataWidth.W)
